dc6cb8b684638c3129adc8fd72f22f775d8c4b92
Alexey Maenkov <alexeymaenkov@gmail.com>
2026-02-19 23:42:47 +0300

    innitial commit

---
+++ Homework_Module_4_Function/Task3_Shuffle/Shuffle.cs
@@ -0,0 +1,12 @@
+namespace Homework_Module_4_Function.Task3_Shuffle;
+
+public class Shuffle
+{
+    public void Run()
+    {
+        //Реализуйте функцию Shuffle, которая перемешивает элементы
+        //массива в случайном порядке.
+
+
+    }
+}
